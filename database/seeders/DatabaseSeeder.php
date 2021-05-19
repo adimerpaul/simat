3be5c2ccb88f6bbee77a15a3bde7434b59e1f698
Alejandro <lopez.gutierrez.a07@gmail.com>
2021-05-19 10:53:20 -0400

    SEED

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert(['name'=>'Jorge Cox','username'=>'JOCOX','password'=>Hash::make('JO123456')]);
         DB::table('users')->insert(['name'=>'Amilcar Quispaya','username'=>'AMQUISPAYA','password'=>Hash::make('AM123456')]);
         DB::table('users')->insert(['name'=>'Pablo Pacheco','username'=>'PAPACHECO','password'=>Hash::make('PA123456')]);
+        DB::table('users')->insert(['name'=>'Carola Ledezma','username'=>'CALEDEZMA','password'=>Hash::make('CA123456')]);
 
         // \App\Models\User::factory(10)->create();
     }
